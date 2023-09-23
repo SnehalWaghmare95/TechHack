5884fab4db89ea930e9606d6d61081861cff1b05
snehalwaghmare095@outlook.com <snehalwaghmare095@outlook.com>
2023-09-23 18:26:40 +0530

    Creating the Json web token

---
+++ JWTImplementation/Controllers/Auth.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace JWTImplementation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Auth : ControllerBase
+    {
+        IConfiguration _builder;
+        public Auth(IConfiguration builder) { _builder = builder; }
+       
+
+        [HttpGet("GetToken")]
+        // Method to get the token
+        public IActionResult GetToken() {
+            try
+            {
+                var config = _builder;
+                var ValidIssuer = config["JWTSettings:Issuer"];
+                var ValidAudience = config["JWTSettings:Audiance"];
+                // Hardcoading here as  this has be kept in db,
+                // we are restricting the scope of this tutorial to only token creation and validation
+
+                string username = "User123";
+                string password = "password@123";
+                var IssuerSigningKey = Encoding.ASCII.GetBytes(config["JWTSettings:Key"]);
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new[] {
+                         new Claim("Id", Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Sub,username ),
+                new Claim(JwtRegisteredClaimNames.Email,username ),
+                new Claim(JwtRegisteredClaimNames.Jti,
+                Guid.NewGuid().ToString())
+                    }),
+                    Expires = DateTime.UtcNow.AddMinutes(10),
+                    Issuer = ValidIssuer,
+                    Audience = ValidAudience,
+                    SigningCredentials = new SigningCredentials
+                (new SymmetricSecurityKey(IssuerSigningKey),
+                SecurityAlgorithms.HmacSha512Signature),
+                };
+                var tokenHandeler = new JwtSecurityTokenHandler();
+                var token = tokenHandeler.CreateToken(tokenDescriptor);
+                var jwtToken = tokenHandeler.WriteToken(token);
+                return Ok(jwtToken);
+            }
+            catch(Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
--- JWTImplementation/Program.cs
+++ JWTImplementation/Program.cs
@@ -1,8 +1,35 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
-
+//builder.Services.AddScoped
+var config = builder.Configuration;
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddAuthentication(options => {
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(jwtScheme => {
+    jwtScheme.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = config["JWTSettings:Issuer"],
+        ValidAudience = config["JWTSettings:Audiance"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JWTSettings:Key"])),
+        ValidateAudience = true,
+        ValidateIssuer = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true
+    };
+    
+ });
+builder.Services.AddAuthorization(options =>
+{
+
+}); 
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,7 +44,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
